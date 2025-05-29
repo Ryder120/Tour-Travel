fb5580e5e229261b1679ca872ee83751ccfcf927
Ryder120 <buzzrocky35@gmail.com>
2025-05-29 20:19:21 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './Component/Navbar'
+import Cities from './Component/Cities'
 
 const App = () => {
   return (
     <>
       <Navbar />
-    
-      
+      <Cities />
+  
       </>
       
     
